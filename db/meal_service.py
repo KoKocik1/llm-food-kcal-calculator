26a73bf0c429321cbc6add50d5cefa1c2ef9e66f
Krzysztof Kokot <krzysiukokot@interia.pl>
2025-04-03 17:06:02 +0200

    Meal Database Refactor

--- db/meal_service.py
+++ db/meal_service.py
@@ -4,14 +4,15 @@ import os
 from model.meal import Meal
 from model.mealCreate import MealCreate
 from datetime import datetime
-from typing import List
+from typing import List, Dict, Any, Optional
 from mappers.meal_mappers import collections_to_meals
 from tabulate import tabulate
+from langchain.tools import tool
+from bson import ObjectId
 
 
-def get_meals_from_date_range(
-    start_date: datetime, end_date: datetime = datetime.now()
-) -> List[Meal]:
+@tool
+def get_meals_by_date_range(start_date: str, end_date: str = None) -> List[Meal]:
     """Get all meals from a given date onwards.
 
     Args:
@@ -22,55 +23,95 @@ def get_meals_from_date_range(
     """
     db = get_mongo_db()
     collection_meals = db[os.getenv("MONGO_COLLECTION_MEALS")]
+    start_date = datetime.strptime(start_date, "%Y-%m-%d")
+
+    if end_date:
+        end_date = datetime.now().strftime("%Y-%m-%d")
+    else:
+        end_date = start_date
+
     start_of_day = datetime(start_date.year, start_date.month, start_date.day)
-    end_of_day = datetime(end_date.year, end_date.month, end_date.day, 23, 59, 59)
+    end_of_day = datetime(end_date.year, end_date.month,
+                          end_date.day, 23, 59, 59)
     collections = collection_meals.find(
         {"date": {"$gte": start_of_day, "$lte": end_of_day}}
     )
     return collections_to_meals(collections)
 
 
-def get_meal_by_day(date: datetime) -> List[Meal]:
-    """Get all meals for a specific day.
+@tool
+def get_meals_by_day(date: str = datetime.now().strftime("%Y-%m-%d")) -> List[Meal]:
+    """Get all meals for a specific day. If no date is provided, it defaults to today.
+    The date is formatted as YYYY-MM-DD. 
 
     Args:
-        date (datetime): Date to get meals for
+        date (str): Date to get meals for (default is today)
 
     Returns:
         List[Meal]: List of meals for that day
     """
-    start_of_day = datetime(date.year, date.month, date.day)
-    end_of_day = datetime(date.year, date.month, date.day, 23, 59, 59)
-
-    return get_meals_from_date_range(start_of_day, end_of_day)
+    return get_meals_by_date_range(date)
 
 
-def create_meal(meal: MealCreate) -> None:
+@tool
+def get_meal_by_id(id: str) -> Optional[Meal]:
+    """Get a meal by its id.
+
+    Args:
+        id (str): Id of the meal to get
+
+    Returns:
+        Optional[Meal]: Meal object
+    """
+    db = get_mongo_db()
+    collection_meals = db[os.getenv("MONGO_COLLECTION_MEALS")]
+    meal = collection_meals.find_one({"_id": id})
+    if meal:
+        return Meal(**meal)
+    return None
+
+
+@tool
+def create_meal(meal: MealCreate) -> Dict[str, Any]:
     """Create a new meal in the database.
 
     Args:
-        meal (Meal): Meal object to create
+        meal (MealCreate): Meal object to create
+
+    Returns:
+        Dict[str, Any]: Meal object with _id
     """
     db = get_mongo_db()
     collection_meals = db[os.getenv("MONGO_COLLECTION_MEALS")]
     meal_dict = meal.to_dict()
-    collection_meals.insert_one(meal_dict)
-    print(f"Created meal: {meal_dict}")
+    result = collection_meals.insert_one(meal_dict)
+    meal_dict["_id"] = result.inserted_id
+    print(f"Created meal: {meal_dict} with id: {result.inserted_id}")
+    return meal_dict
 
 
-def edit_meal(id: str, meal: MealCreate) -> None:
+@tool
+def update_meal(id: str, meal: MealCreate) -> Optional[Dict[str, Any]]:
     """Edit an existing meal in the database.
 
     Args:
         id (str): MongoDB _id of meal to edit
-        meal (Meal): Updated meal object
+        meal (MealCreate): Updated meal object
+
+    Returns:
+        Optional[Dict[str, Any]]: Updated meal object with _id
     """
     db = get_mongo_db()
     collection_meals = db[os.getenv("MONGO_COLLECTION_MEALS")]
-    collection_meals.update_one({"_id": id}, {"$set": meal.to_dict()})
+    result = collection_meals.update_one(
+        {"_id": id}, {"$set": meal.to_dict()})
+    if result.modified_count > 0:
+        return get_meal_by_id(id)
+    return None
 
 
-def delete_meal(id: str) -> None:
+@tool
+def delete_meal(id: str) -> str:
     """Delete a meal from the database.
 
     Args:
@@ -78,7 +119,17 @@ def delete_meal(id: str) -> None:
     """
     db = get_mongo_db()
     collection_meals = db[os.getenv("MONGO_COLLECTION_MEALS")]
-    collection_meals.delete_one({"_id": id})
+    result = collection_meals.delete_one({'_id': ObjectId(id)})
+    return result.deleted_count > 0
+
+
+@tool
+def get_total_kcal(date: str = datetime.now()) -> int:
+    """Get the total kcal for a specific day.
+    """
+    meals = get_meals_by_day(date)
+    total_kcal = sum(meal.calories for meal in meals)
+    return total_kcal
 
 
 if __name__ == "__main__":
@@ -93,13 +144,14 @@ if __name__ == "__main__":
             description="Test Description1",
         )
     )
-    meals = get_meal_by_day(datetime.now())
+    meals = get_meals_by_day(datetime.now())
     created_meal = list(filter(lambda x: x.name == "Test Meal1", meals))
     created_meal_id = created_meal[0]._id
-    print(tabulate([meal.to_dict() for meal in meals], headers="keys", tablefmt="grid"))
+    print(tabulate([meal.to_dict()
+          for meal in meals], headers="keys", tablefmt="grid"))
 
     print("========== Edit Meal ==========")
-    edit_meal(
+    update_meal(
         created_meal_id,
         MealCreate(
             name="Updated Meal1",
@@ -109,10 +161,12 @@ if __name__ == "__main__":
             description="Updated Description1",
         ),
     )
-    meals = get_meal_by_day(datetime.now())
-    print(tabulate([meal.to_dict() for meal in meals], headers="keys", tablefmt="grid"))
+    meals = get_meals_by_day(datetime.now())
+    print(tabulate([meal.to_dict()
+          for meal in meals], headers="keys", tablefmt="grid"))
 
     print("========== Delete Meal ==========")
     delete_meal(created_meal_id)
-    meals = get_meal_by_day(datetime.now())
-    print(tabulate([meal.to_dict() for meal in meals], headers="keys", tablefmt="grid"))
+    meals = get_meals_by_day(datetime.now())
+    print(tabulate([meal.to_dict()
+          for meal in meals], headers="keys", tablefmt="grid"))
--- mappers/meal_mappers.py
+++ mappers/meal_mappers.py
@@ -1,28 +1,30 @@
 from model.meal import Meal
 from typing import List
+from tabulate import tabulate
 
 
 def collection_to_meal(collection: dict) -> Meal:
-    """Convert a MongoDB document to a Meal object.
-
-    Args:
-        collection (dict): MongoDB document representing a meal
-
-    Returns:
-        Meal: Converted Meal object
-    """
+    """Convert a MongoDB document to a Meal object."""
     meal_dict = collection.copy()
     meal = Meal(**meal_dict)
     return meal
 
 
 def collections_to_meals(collections: List[dict]) -> List[Meal]:
-    """Convert multiple MongoDB documents to Meal objects.
-
-    Args:
-        collections (List[dict]): List of MongoDB documents
-
-    Returns:
-        List[Meal]: List of converted Meal objects
-    """
+    """Convert multiple MongoDB documents to Meal objects"""
     return [collection_to_meal(collection) for collection in collections]
+
+
+def print_meals(meals: List[Meal]):
+    """Print a list of Meal objects"""
+    if meals:
+        # Convert Meal objects to dictionaries
+        meals_dict = [meal.to_dict() for meal in meals]
+        print(tabulate(meals_dict, headers="keys", tablefmt="grid"))
+    else:
+        print("No meals found")
+
+
+def print_meal(meal: Meal):
+    """Print a single Meal object"""
+    print(tabulate([meal.to_dict()], headers="keys", tablefmt="grid"))
